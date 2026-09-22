3f4996d783979a93b9154e00158db962c3e573d6
marcos <msa.felipesousa@gmail.com>
2026-09-22 20:45:46 -0300

    lista de jogos front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,21 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Catalogo de games</h1>
-      <div>
-        <h2>${}</h2>
-      </div>
+      <>
+  <div className="jogos-lista">
+    {jogos.map((jogo) => (
+      <div className="jogo-card" key={jogo.id_jogo}>
+        <h3>{jogo.nome}</h3>
 
-    </>
+        <p><strong>Gênero:</strong> {jogo.genero}</p>
+        <p><strong>Plataforma:</strong> {jogo.plataforma}</p>
+        <p><strong>Estoque:</strong> {jogo.estoque}</p>
+        <p><strong>Preço:</strong> R$ {Number(jogo.preco).toFixed(2)}</p>
+
+      </div>
+    ))}
+  </div>
+</>
   )
 }
 
